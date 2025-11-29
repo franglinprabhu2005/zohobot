f720295bd374aa503625543fa9d05e4762c238c7
Franglin Prabhu J <23ucs022@anjaconline.org>
2025-11-29 21:29:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from google import genai
 
 # USE NEW SAFE KEY HERE
-client = genai.Client(api_key="AIzaSyBOF0u5i_x-naxmZoFmrKtnhcSnklZsYTg")
+client = genai.Client(api_key="AIzaSyC3k9rdwJgQf53UwqJKwFTv_JXaHD8CrFQ")
 
 app = FastAPI()
 
